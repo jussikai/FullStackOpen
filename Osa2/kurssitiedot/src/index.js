28c8b99f5d806fb37670bdf69fca1fafa3a1ccae
Jussi Kaivonen <jussi.kaivonen@tuni.fi>
2019-10-27 21:09:20 +0200

    2.4

--- Osa2/kurssitiedot/src/index.js
+++ Osa2/kurssitiedot/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom'
 
 
 const Course = (props) => {
-  console.log(props)
-  //const rows = () => 
   return(
   <div>
     <Header course = {props.course.name} />
@@ -39,36 +37,58 @@ const Total = ({parts}) => {
     const sum = parts.map(part => part.exercises).
     reduce( (s,p) => s+p )
     return (
-        <p>Total of {sum} exercises</p>
+        <h2>Total of {sum} exercises</h2>
     )
 }
 
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
-
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+ 
   return (
     <div>
-      <Course course={course} />
+      {courses.map(course => <Course course={course} />)}
     </div>
   )
 }
