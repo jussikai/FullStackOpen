77fbde3b01941f2406dec43702cf5206d7f6bd6d
Jussi Kaivonen <jussi.kaivonen@tuni.fi>
2019-11-05 21:33:12 +0200

    weather works with errors

--- Osa2/resteu/src/App.js
+++ Osa2/resteu/src/App.js
@@ -2,7 +2,28 @@ import React, {useState,useEffect} from 'react';
 import './App.css';
 import axios from 'axios'
 
+
 const Country = ({country}) =>{
+  const [weather, setweather] = useState({current:[{temperature: 0,
+    wind_speed:0 , wind_dir: "", weather_icons: ""
+  }
+  ]})
+
+  useEffect(() => {
+  axios
+    .get('http://api.weatherstack.com/current', {
+    params: {
+      access_key: 'b56ee267f6b24c4be2935e77bc6c8841',
+      query: country.capital
+      }})
+    .then(response => {
+      console.log('promise fulfilled')
+      console.log(response.data)
+      setweather(response.data)
+    }
+    )
+  },[])
+
 
   return(<div>
     <h1>{country.name}</h1>
@@ -17,13 +38,34 @@ const Country = ({country}) =>{
       alt = ""
       height = "200"
       />
+    <h2>Weather in {country.capital}</h2>
+    <p>temperature: {weather.current.temperature}</p>
+    <img 
+      src={weather.current.weather_icons}
+      alt = ""
+      height = "50"
+      />
+    <p>wind: {weather.current.wind_speed} kph 
+     direction {weather.current.wind_dir}</p>
     </div>)
+
 }
 
+
+/*
+    <p>temperature: {weather.current.temperature}</p>
+    <img 
+      src={weather.weather_icons[0]}
+      alt = ""
+      height = "50"
+      />
+    <p>wind: {weather.current.wind_speed} 
+    kph direction {weather.current.wind_dir}</p>
+    */
 const Data = ({data}) => {
-  console.log(data)
   const Note = ({country}) => 
-  <li key = {country.name}>{country.name}</li>
+  <li key = {country.name}>{country.name} <button>
+    show</button></li>
 
   const rows = () => data.map(country => 
     <Note key = {country.name} country = {country}/>)
@@ -33,25 +75,24 @@ const Data = ({data}) => {
       <div>Too many matches, specify another filter</div>
     )
   }
-  else if(data.length===1){
-    return(<div><Country country = {data[0]}/></div>)
+  else if(data.length !== 1){
+    return(<div>
+      <ul>
+        {rows()}
+      </ul>
+    </div>)
   }
-  return(<div>
-    <ul>
-      {rows()}
-    </ul>
-  </div>)
+  return(<div><Country country = {data[0]}/></div>)
+
 
 }
 
 function App() {
   const [filter, setFilter] = useState('')
   const [data, setData] = useState([])
-  
-
 
   useEffect(() => {
-    console.log('effect')
+
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
@@ -65,8 +106,6 @@ function App() {
 
   const toShow = data.filter(country => country.name.toLowerCase()
     .includes(filter.toLowerCase()))
-  
-  console.log(toShow)
   return (
     <div>
       <form>
