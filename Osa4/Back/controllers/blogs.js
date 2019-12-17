5a70c95922c313de8b9566dcc73cb124a0e14dbb
Jussi Kaivonen <jussi.kaivonen@tuni.fi>
2019-12-17 20:28:08 +0200

    4.12 asti tehty

--- Osa4/Back/controllers/blogs.js
+++ Osa4/Back/controllers/blogs.js
@@ -13,11 +13,20 @@ blogsRouter.get('/', (request, response) => {
 blogsRouter.post('/', (request, response) => {
 const blog = new Blog(request.body)
 
-blog
-    .save()
-    .then(result => {
-    response.status(201).json(result)
-    })
+blog.likes = blog.likes?blog.likes:0
+console.log(blog.title)
+if(!blog.title || !blog.url){
+  response.status(400).json(result)
+  }
+else{
+  console.log('taal ollaan')
+  blog
+      .save()
+      .then(result => 
+      response.status(201).json(result)
+      )
+}
 })
+
 
 module.exports = blogsRouter
--- Osa4/Back/models/blog.js
+++ Osa4/Back/models/blog.js
@@ -4,7 +4,16 @@ const blogSchema = mongoose.Schema({
     title: String,
     author: String,
     url: String,
-    likes: Number
+    likes: Number,
+    id: String
   })
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
 module.exports = mongoose.model('Blog', blogSchema)
