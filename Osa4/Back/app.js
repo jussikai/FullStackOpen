edcfa22f2040a620385e3b56e7136329ea0258c5
Jussi Kaivonen <jussi.kaivonen@tuni.fi>
2019-12-18 22:08:07 +0200

    Osa4 done

--- Osa4/Back/app.js
+++ Osa4/Back/app.js
@@ -5,8 +5,11 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const notesRouter = require('./controllers/blogs')
-//const middleware = require('./utils/middleware')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware.js')
 const mongoose = require('mongoose')
+
 
 
 
@@ -24,11 +27,12 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(bodyParser.json())
 //app.use(middleware.requestLogger)
-
+app.use(middleware.tokenExtractor)
 app.use('/api/blogs', notesRouter)
-
-//app.use(middleware.unknownEndpoint)
-//app.use(middleware.errorHandler)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
 
--- Osa4/Back/controllers/blogs.js
+++ Osa4/Back/controllers/blogs.js
@@ -1,48 +1,80 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
+
+
+
+blogsRouter.get('/', async (request, response,next) => {
+    try{
+      blogs = await Blog
+      .find({}).populate('blogs').populate('user',{username:1,name:1})
+      response.json(blogs)
+    } catch(exception){
+      next(exception)
+    }
   })
   
-blogsRouter.post('/', (request, response) => {
-const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response,next) => {
+  const blog = new Blog(request.body)
 
-blog.likes = blog.likes?blog.likes:0
-console.log(blog.title)
-if(!blog.title || !blog.url){
-  response.status(400).json(result)
+  if(!blog.title || !blog.url){
+    response.status(400)
+    }
+  else{
+    try {
+      const decodedToken = jwt.verify(request.token, process.env.SECRET)
+      if (!request.token || !decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+      }
+
+      const user = await User.findById(decodedToken.id)
+
+      blog.likes = blog.likes?blog.likes:0
+      blog.user = user._id
+
+      console.log(blog.title)
+        const savedBlog = await blog.save()
+        user.blogs = user.blogs.concat(savedBlog._id)
+        await user.save()
+        response.status(201).json(savedBlog)
+      }catch(exception){
+        next(exception)
+      }
   }
-else{
-  console.log('taal ollaan')
-  blog
-      .save()
-      .then(result => 
-      response.status(201).json(result)
-      )
-}
 })
 
-blogsRouter.delete('/:id',async (request, response) => {
-  try{await Blog.findByIdAndRemove(request.params.id)
-  response.status(204).end()}
+blogsRouter.delete('/:id',async (request, response,next) => {
+  try{
+    const blog = await Blog.findById(request.params.id)
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!request.token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    if (!blog){
+      return response.status(404).end()
+    }
+    if (decodedToken.id.toString() === blog.user.toString()){
+      await Blog.findByIdAndRemove(request.params.id)
+      response.status(204).end()}  
+    else{
+      response.status(401).json({ error: 'token missing or invalid' })
+    }
+  }
   catch(exception){
-    response.status(404).end()
+    next(exception)
   }
 })
 
-blogsRouter.put('/:id',async (request, response) => {
+blogsRouter.put('/:id',async (request, response,next) => {
   blog = request.body
   blog.likes = blog.likes?blog.likes:0
   try{updated = await Blog.findByIdAndUpdate(request.params.id,blog,{new:true})
   response.json(updated.toJSON())}
   catch(exception){
-    response.status(404).end()
+    next(exception)
   }
 })
 
