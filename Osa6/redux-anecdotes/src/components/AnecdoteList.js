ca80606ae1dd0b3df1268b5b4fe60bcc32cfb55e
Jussi Kaivonen <jussi.kaivonen@ideal.fi>
2019-12-22 22:28:55 +0200

    6.11 asti tehty

--- Osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ Osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import {voteAnecdote} from '../reducers/anecdoteReducer'
+import {stopNotification} from '../reducers/notificationReducer'
 
 const AnecdoteList= (props)=>{
-    const anecdotes = props.store.getState()
+    const {anecdotes, notification, filter} = props.store.getState()
+    const toShow = anecdotes.filter(anecdote => anecdote.content.toLowerCase()
+    .includes(filter.toLowerCase()) )
 
     const vote = (id) => {
       console.log('vote', id)
       props.store.dispatch(voteAnecdote(id))
+      setTimeout(()=>{props.store.dispatch(stopNotification())},5000)
     }
     return(<div>
-        {anecdotes.map(anecdote =>
+        {toShow.map(anecdote =>
             <div key={anecdote.id}>
               <div>
                 {anecdote.content}
